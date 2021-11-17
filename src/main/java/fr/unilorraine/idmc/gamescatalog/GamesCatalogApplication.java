caca5a4f7826c78d4856225123cd1fe173331941
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-11-17 13:04:34 +0100

    initial commit

---
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/GamesCatalogApplication.java
@@ -0,0 +1,13 @@
+package fr.unilorraine.idmc.gamescatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/unilorraine/idmc/gamescatalog/GamesCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.unilorraine.idmc.gamescatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
