9b3178fc7dc1a3a128e00bd7a1822861f09ffed1
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-11-24 13:43:56 +0100

    added custom property

--- src/main/java/fr/unilorraine/idmc/gamescatalog/client/PostmanEchoJDK11Client.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/client/PostmanEchoJDK11Client.java
@@ -1,6 +1,9 @@
 package fr.unilorraine.idmc.gamescatalog.client;
 
+import fr.unilorraine.idmc.gamescatalog.config.PostmanProperties;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -9,12 +12,15 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 @Component
+@RequiredArgsConstructor
 public class PostmanEchoJDK11Client {
+
+    private final PostmanProperties properties;
 
     @SneakyThrows
     public String testGet() {
         var request = HttpRequest.newBuilder()
-                .uri(new URI("https://postman-echo.com/get"))
+                .uri(new URI(properties.getEchoUrl()))
                 .GET()
                 .build();
 
