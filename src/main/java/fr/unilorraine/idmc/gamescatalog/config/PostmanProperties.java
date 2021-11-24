e096e4966e28d1623bce591a95d7ac70499d0f65
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-11-24 13:55:12 +0100

    added static resource

--- src/main/java/fr/unilorraine/idmc/gamescatalog/config/PostmanProperties.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/config/PostmanProperties.java
@@ -2,6 +2,7 @@ package fr.unilorraine.idmc.gamescatalog.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Component;
 @Setter
 public class PostmanProperties {
 
+    @Value("${app.postman.echo-url}")
     private String echoUrl;
+
     private String field1;
     private String field2;
     private String field3;
